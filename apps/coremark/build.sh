3ff0b47e4fbfeca2209ce1b7b2bc668b6c39f07b
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2024-08-25 01:33:05 +0300

    Cleanup

--- apps/coremark/build.sh
+++ apps/coremark/build.sh
@@ -1,6 +1,7 @@
 # Compile
 clang-16 --target=wasm32 --no-standard-libraries -Wl,--no-entry \
     -Wl,--stack-first -Wl,-z,stack-size=512 \
+    -mbulk-memory -mnontrapping-fptoint -msign-ext \
     -g0 -s -O3 -DFLAGS_STR="-O3" -I./ -o app.wasm \
     core_list_join.c core_main.c core_matrix.c core_state.c core_util.c core_portme.c cvt.c
 
